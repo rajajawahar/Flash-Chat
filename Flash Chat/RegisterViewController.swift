a402d5bcd1931aba5e1834956631de960c25942e
Raja Jawahar <rajajawahar77@gmail.com>
2018-07-30 17:50:47 +0530

    SignOut function added

--- Flash Chat/RegisterViewController.swift
+++ Flash Chat/RegisterViewController.swift
@@ -6,13 +6,14 @@
 //
 
 import UIKit
+import Firebase
 
 
 class RegisterViewController: UIViewController {
-
+    
     
     //Pre-linked IBOutlets
-
+    
     @IBOutlet var emailTextfield: UITextField!
     @IBOutlet var passwordTextfield: UITextField!
     
@@ -20,23 +21,21 @@ class RegisterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-
-  
+    
+    
     @IBAction func registerPressed(_ sender: AnyObject) {
-        
-
-        
-        //TODO: Set up a new user on our Firbase database
-        
-        
-
-        
-        
+        Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
+            if error != nil{
+                print(error.debugDescription)
+            }
+            print("Registration Successful")
+            self.performSegue(withIdentifier: "goToChat", sender: self)
+        }
     } 
     
     
